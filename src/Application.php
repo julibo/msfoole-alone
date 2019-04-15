76fc9fe9f6dc48b2e8b842426fed9801d1ad9912
carson <yuzhanwei@g7.com.cn>
2019-04-15 09:24:56 +0800

    历史构建

---
+++ src/Application.php
@@ -0,0 +1,206 @@
+<?php
+// +----------------------------------------------------------------------
+// | msfoole [ 基于swoole4的高性能API服务框架 ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018 http://julibo.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: carson <yuzhanwei@aliyun.com>
+// +----------------------------------------------------------------------
+
+namespace Julibo\Msfoole;
+
+use Swoole\Http\Request as SwooleRequest;
+use Swoole\Http\Response as SwooleResponse;
+use Julibo\Msfoole\Facade\Log;
+use Julibo\Msfoole\Facade\Config;
+
+class Application
+{
+    /**
+     * http请求
+     * @var
+     */
+    private $httpRequest;
+
+    /**
+     * http应答
+     * @var
+     */
+    private $httpResponse;
+
+    // 开始时间和内存占用
+    private $beginTime;
+    private $beginMem;
+
+    /**
+     * @var 通道
+     */
+    private $chan;
+
+    /**
+     * @var
+     */
+    private $cookie;
+
+    /**
+     * Application constructor.
+     * @param SwooleRequest $request
+     * @param SwooleResponse $response
+     * @param Channel $chan
+     */
+    public function __construct(SwooleRequest $request, SwooleResponse $response, Channel $chan)
+    {
+        $this->beginTime = microtime(true);
+        $this->beginMem  = memory_get_usage();
+        $this->chan = $chan;
+        $this->httpResponse = new Response($response);
+        $this->httpRequest = new HttpRequest($request);
+        $this->init();
+    }
+
+    /**
+     * 初始化
+     * @return mixed
+     */
+    private function init()
+    {
+        $this->httpRequest->init();
+        $this->httpRequest->explain();
+        $this->cookie = new Cookie($this->httpRequest, $this->httpResponse);
+        Log::setEnv($this->httpRequest)->info('请求开始，请求参数为 {message}', ['message' => json_encode($this->httpRequest->params)]);
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function checkRequest()
+    {
+        $header = $this->httpRequest->getHeader();
+        if (empty($header) || empty($header['level']) || empty($header['timestamp']) || empty($header['token']) ||
+            empty($header['signer']) || $header['level'] != 2 || $header['timestamp'] > strtotime('10 minutes') * 1000 ||
+            $header['timestamp'] < strtotime('-10 minutes') * 1000) {
+            throw new Exception(Prompt::$common['REQUEST_EXCEPTION']['msg'], Prompt::$common['REQUEST_EXCEPTION']['code']);
+        }
+        $signSrc = $header['timestamp'].$header['token'];
+        if (!empty($this->params)) {
+            ksort($this->params);
+            array_walk($this->params, function($value,$key) use (&$signSrc) {
+                $signSrc .= $key.$value;
+            });
+        }
+        if (md5($signSrc) != $header['signer']) {
+            throw new Exception(Prompt::$common['SIGN_EXCEPTION']['msg'], Prompt::$common['SIGN_EXCEPTION']['code']);
+        }
+        return $this;
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function checkToken()
+    {
+        $token = $this->httpRequest->getHeader('token');
+        if (empty($token)) {
+            throw new Exception(Prompt::$common['UNLAWFUL_TOKEN']['msg'], Prompt::$common['UNLAWFUL_TOKEN']['code']);
+        } else {
+            $default = Config::get('application.default.key');
+            if ($default != $token) {
+                $user = $this->cookie->getTokenCache($token);
+                if (empty($user)) {
+                    throw new Exception(Prompt::$common['UNLAWFUL_TOKEN']['msg'], Prompt::$common['UNLAWFUL_TOKEN']['code']);
+                }
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * 处理http请求
+     */
+    public function handling()
+    {
+        try {
+            ob_start();
+            # step 1 验证请求合法性(有令牌则不验证)
+            if (empty($this->httpRequest->getHeader('permit')) ||
+                $this->httpRequest->getHeader('permit') != Config::get('msfoole.health.permit')) {
+                $this->checkToken()->checkRequest();
+            }
+            # step 2 调用服务
+            $this->working();
+            $content = ob_get_clean();
+            $this->httpResponse->end($content);
+        } catch (\Throwable $e) {
+            if ($e->getCode() == 0) {
+                $code = 911;
+            } else {
+                $code = $e->getCode();
+            }
+            if (Config::get('application.debug')) {
+                $content = ['code' => $code, 'msg'=>$e->getMessage(), 'identification' => $this->httpRequest->identification,
+                    'extra'=>['file'=>$e->getFile(), 'line'=>$e->getLine()]];
+            } else {
+                $content = ['code' => $code, 'msg'=>$e->getMessage(), 'identification' => $this->httpRequest->identification];
+            }
+            $this->httpResponse->end(json_encode($content));
+            if ($e->getCode() >= 1000) {
+                throw $e;
+            }
+        }
+    }
+
+    /**
+     * 运行请求
+     * @throws Exception
+     */
+    public function working()
+    {
+        $controller = Loader::factory($this->httpRequest->controller, $this->httpRequest->namespace, $this->httpRequest, $this->cookie, $this->chan);
+        if(!is_callable(array($controller, $this->httpRequest->action))) {
+            throw new Exception(Prompt::$common['METHOD_NOT_EXIST']['msg'], Prompt::$common['METHOD_NOT_EXIST']['code']);
+        }
+        $data = call_user_func([$controller, $this->httpRequest->action]);
+        if ($data === null && ob_get_contents() != '') {
+        } else {
+            if (Config::get('application.debug')) {
+                $executionTime = round(microtime(true) - $this->beginTime, 6) . 's';
+                $consumeMem = round((memory_get_usage() - $this->beginMem) / 1024, 2) . 'K';
+                $result = ['code' => 0, 'data' => $data, 'identification' => $this->httpRequest->identification,
+                    'executionTime' =>$executionTime, 'consumeMem' => $consumeMem ];
+            } else {
+                $result = ['code' => 0, 'data' => $data, 'identification' => $this->httpRequest->identification];
+            }
+            echo json_encode($result);
+        }
+    }
+
+
+
+    /**
+     * 释放资源
+     * @return mixed
+     */
+    private function destruct()
+    {
+        $executionTime = round(microtime(true) - $this->beginTime, 6) . 's';
+        $consumeMem = round((memory_get_usage() - $this->beginMem) / 1024, 2) . 'K';
+        Log::setEnv($this->httpRequest)->info('请求结束，执行时间{executionTime}，消耗内存{consumeMem}', ['executionTime' => $executionTime, 'consumeMem' => $consumeMem]);
+        if ($executionTime > Config::get('log.slow_time')) {
+            Log::setEnv($this->httpRequest)->slow('当前方法执行时间{executionTime}，消耗内存{consumeMem}', ['executionTime' => $executionTime, 'consumeMem' => $consumeMem]);
+        }
+        unset($this->httpRequest, $this->httpResponse, $this->chan, $this->cookie);
+
+    }
+
+    /**
+     * 释放资源
+     */
+    public function __destruct()
+    {
+        $this->destruct();
+
+    }
+
+}
---
+++ src/Server/HttpServer.php
@@ -0,0 +1,378 @@
+<?php
+// +----------------------------------------------------------------------
+// | msfoole [ 基于swoole4的高性能API服务框架 ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018 http://julibo.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: carson <yuzhanwei@aliyun.com>
+// +----------------------------------------------------------------------
+
+namespace Julibo\Msfoole\Server;
+
+use Swoole\Process;
+use Swoole\Table;
+use Swoole\Http\Request as SwooleRequest;
+use Swoole\Http\Response as SwooleResponse;
+use Swoole\Websocket\Server as Websocket;
+use Swoole\WebSocket\Frame as Webframe;
+use Julibo\Msfoole\Facade\Config;
+use Julibo\Msfoole\Facade\Cache;
+use Julibo\Msfoole\Facade\Log;
+use Julibo\Msfoole\Channel;
+use Julibo\Msfoole\Helper;
+use Julibo\Msfoole\HttpClient;
+use Julibo\Msfoole\Application;
+use Julibo\Msfoole\Interfaces\Server as BaseServer;
+
+class HttpServer extends BaseServer
+{
+    /**
+     * SwooleServer类型
+     * @var string
+     */
+    protected $serverType = 'http';
+
+    /**
+     * 支持的响应事件
+     * @var array
+     */
+    protected $event = [
+        'Start',
+        'Shutdown',
+        'ManagerStart',
+        'ManagerStop',
+        'WorkerStart',
+        'WorkerStop',
+        'WorkerExit',
+        'WorkerError',
+        'Close',
+        'Request',
+        'Open',
+        'Message',
+    ];
+
+    /**
+     * @var 通道
+     */
+    protected $chan;
+
+    /**
+     * 客户端连接内存表
+     */
+    protected $table;
+
+    /**
+     * @var 应用名
+     */
+    protected $appName;
+
+    /**
+     * @var string 健康检查IP
+     */
+    protected $health_host;
+
+    /**
+     * @var 健康检查计数器
+     */
+    protected $counter = 0;
+
+    /**
+     * @var string 健康检查URI
+     */
+    protected $health_uri;
+
+    /**
+     * @var 健康检查许可证
+     */
+    protected $health_permit;
+
+    /**
+     * 初始化
+     */
+    protected function init()
+    {
+        $this->appName = Config::get('application.name') ?? '';
+        $this->option['upload_tmp_dir'] = TEMP_PATH;
+        $this->option['http_parse_post'] = true;
+        $this->option['http_compression'] = true;
+        $config = Config::get('msfoole') ?? [];
+        unset($config['host'], $config['port'], $config['ssl'], $config['option']);
+        $this->config = array_merge($this->config, $config);
+        $this->health_host = $this->config['health']['host'] ?? '127.0.0.1';
+        $this->health_uri = $this->config['health']['uri'] ?? '/Index/Index/health';
+        $this->health_permit = $this->config['health']['permit'] ?? '700040d41f47592c';
+        if ($this->pattern) {
+            $this->serverType = 'socket';
+        }
+    }
+
+    /**
+     * 启动辅助逻辑
+     */
+    protected function startLogic()
+    {
+        # 初始化缓存
+        $cacheConfig = Config::get('cache.default') ?? [];
+        Cache::init($cacheConfig);
+        # 开启异步定时监控
+        $this->monitorProcess();
+        # 创建客户端连接内存表
+        $this->createTable();
+    }
+
+    /**
+     * 创建客户端连接内存表
+     */
+    private function createTable()
+    {
+        if ($this->serverType == 'socket') {
+            $this->table = new table($this->config['table']['size'] ?? 1024);
+            $this->table->column('token', table::TYPE_STRING, 32); // 用户标识符
+            $this->table->column('counter', table::TYPE_INT, 4); // 计数器
+            $this->table->column('create_time', table::TYPE_INT, 4); // 创建时间戳
+            $this->table->column('update_time', table::TYPE_INT, 4); // 更新时间戳
+            $this->table->column('user_info', table::TYPE_STRING, 1024); // 用户信息描述
+            $this->table->create();
+        }
+    }
+
+    /**
+     * 文件监控，不包含配置变化
+     */
+    private function monitorProcess()
+    {
+        $monitorConfig = $this->config['monitor'] ?? [];
+        $switch = $monitorConfig['switch'] ?? false;
+        $paths = $monitorConfig['path'] ?? null;
+        if ($switch && $paths) {
+            $monitor = new Process(function (Process $process) use ($monitorConfig) {
+                // echo "文件监控进程启动";
+                Helper::setProcessTitle("msfoole:monitor-" . $this->appName);
+                $timer = $monitorConfig['interval'] ?? 3;
+                $paths = $monitorConfig['path'];
+                swoole_timer_tick($timer * 1000, function () use($paths) {
+                    if (!is_array($paths)) {
+                        $paths = array($paths);
+                    }
+                    foreach ($paths as $path) {
+                        $path = ROOT_PATH . $path;
+                        if (!is_dir($path)) {
+                            continue;
+                        }
+                        $dir      = new \RecursiveDirectoryIterator($path);
+                        $iterator = new \RecursiveIteratorIterator($dir);
+                        foreach ($iterator as $file) {
+                            if (pathinfo($file, PATHINFO_EXTENSION) != 'php') {
+                                continue;
+                            }
+                            if ($this->lastMtime < $file->getMTime()) {
+                                $this->lastMtime = $file->getMTime();
+                                echo '[update]' . $file . " reload...\n";
+                                $this->swoole->reload();
+                                break 2;
+                            }
+                        }
+                    }
+                });
+            });
+            $this->swoole->addProcess($monitor);
+        }
+    }
+
+    /**
+     * @param \Swoole\Server $server
+     */
+    public function onStart(\Swoole\Server $server)
+    {
+        // echo "主进程启动";
+        Helper::setProcessTitle("msfoole:master-" . $this->appName);
+    }
+
+    /**
+     * @param \Swoole\Server $server
+     */
+    public function onShutdown(\Swoole\Server $server)
+    {
+        // echo "主进程结束";
+        $tips = sprintf("【%s:%s】主进程结束", $this->host, $this->port);
+        Helper::sendDingRobotTxt($tips);
+    }
+
+    /**
+     * @param \Swoole\Server $server
+     */
+    public function onManagerStart(\Swoole\Server $server)
+    {
+        // echo "管理进程启动";
+        Helper::setProcessTitle("msfoole:manager-" . $this->appName);
+    }
+
+    /**
+     * @param \Swoole\Server $server
+     */
+    public function onManagerStop(\Swoole\Server $server)
+    {
+        // echo "管理进程停止";
+        $tips = sprintf("【%s:%s】管理进程停止", $this->host, $this->port);
+        Helper::sendDingRobotTxt($tips);
+    }
+
+    /**
+     * @param \Swoole\Server $server
+     * @param int $worker_id
+     */
+    public function onWorkerStop(\Swoole\Server $server, int $worker_id)
+    {
+        // echo "worker进程终止";
+        $tips = sprintf("【%s:%s】worker进程终止", $this->host, $this->port);
+        Helper::sendDingRobotTxt($tips);
+    }
+
+    /**
+     * @param \Swoole\Server $server
+     * @param int $worker_id
+     */
+    public function onWorkerExit(\Swoole\Server $server, int $worker_id)
+    {
+        // echo "worker进程退出";
+        $tips = sprintf("【%s:%s】worker进程退出", $this->host, $this->port);
+        Helper::sendDingRobotTxt($tips);
+    }
+
+    /**
+     * @param \Swoole\Server $serv
+     * @param int $worker_id
+     * @param int $worker_pid
+     * @param int $exit_code
+     * @param int $signal
+     */
+    public function onWorkerError(\Swoole\Server $serv, int $worker_id, int $worker_pid, int $exit_code, int $signal)
+    {
+        $error = sprintf("worker进程异常:[%d] %d 退出的状态码为%d, 退出的信号为%d", $worker_pid, $worker_id, $exit_code, $signal);
+        Helper::sendDingRobotTxt($error);
+    }
+
+    /**
+     * @param \Swoole\Server $server
+     * @param int $fd
+     * @param int $reactorId
+     */
+    public function onClose(\Swoole\Server $server, int $fd, int $reactorId)
+    {
+        // echo sprintf('%s的连接关闭', $fd);
+        // 销毁客户连接内存表记录
+        if (!is_null($this->table) && $this->table->exist($fd)) {
+            $this->table->del($fd);
+        }
+    }
+
+    /**
+     * Worker进程启动回调
+     * @param \Swoole\Server $server
+     * @param int $worker_id
+     */
+    public function onWorkerStart(\Swoole\Server $server, int $worker_id)
+    {
+        // echo "worker进程启动";
+        Helper::setProcessTitle("msfoole:worker-" . $this->appName);
+        // step 0 健康检查
+        if ($worker_id == 0) {
+            swoole_timer_tick(1000, function () use($server) {
+                $cli = new HttpClient($this->health_host, $this->port, $this->health_permit);
+                $result = $cli->get($this->health_uri);
+                if (empty($result) || empty($result['statusCode']) || $result['statusCode'] != 200) {
+                    $this->counter++;
+                }
+                if ($this->counter > 3) {
+                    $server->shutdown();
+                }
+            });
+        }
+        // step 1 创建通道
+        $chanConfig = $this->config['channel'];
+        $capacity = $chanConfig['capacity'] ?? 100;
+        $this->chan = Channel::instance($capacity);
+        // step 2 开启日志通道
+        Log::setChan($this->chan);
+        // step 3 创建协程工作池
+        $this->WorkingPool();
+    }
+
+    /**
+     * 工作协程
+     * 负责从通道中消费数据并进行异步处理
+     */
+    private function WorkingPool()
+    {
+        go(function () {
+            while(true) {
+                $data = $this->chan->pop();
+                if (!empty($data) && is_int($data['type'])) {
+                    switch ($data['type']) {
+                        case 2:
+                            // 执行自定义方法
+                            if ($data['class'] && $data['method']) {
+                                $parameter = $data['parameter'] ?? [];
+                                call_user_func_array([$data['class'], $data['method']], $parameter);
+                            }
+                            break;
+                        default:
+                            // 写入日志记录
+                            if (!empty($data['log'])) {
+                                Log::saveData($data['log']);
+                            }
+                            break;
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     * request回调
+     * @param SwooleRequest $request
+     * @param SwooleResponse $response
+     * @throws \Throwable
+     */
+    public function onRequest(SwooleRequest $request, SwooleResponse $response)
+    {
+        // 执行应用并响应
+        // print_r($request);
+        $uri = $request->server['request_uri'];
+        if ($uri == '/favicon.ico') {
+            $response->status(404);
+            $response->end();
+        } else {
+            # todo 跨域
+            $app = new Application($request, $response, $this->chan);
+            $app->handling();
+        }
+    }
+
+    /**
+     * 连接开启回调
+     * @param Websocket $server
+     * @param SwooleRequest $request
+     */
+    public function WebsocketonOpen(Websocket $server, SwooleRequest $request)
+    {
+        // 开启websocket连接
+        // print_r($request);
+
+    }
+
+    /**
+     * Message回调
+     * @param $server
+     * @param $frame
+     */
+    public function WebsocketonMessage(Websocket $server, Webframe $frame)
+    {
+        // 执行应用并响应
+        // print_r("receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}");
+
+    }
+
+}
